40698e54345b0eb9c0595a1b36a36373de004740
Sergio Dejo <sdejoame@uci.edu>
2020-04-22 15:19:50 -0700

    fixed extract next links function

--- scraper.py
+++ scraper.py
@@ -1,56 +1,34 @@
 import re
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
+import sys
 
-DBDictionary = dict()
-MaxTokens = int
-MaxURL = str
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
+
     return [link for link in links if is_valid(link)]
 
-
 def extract_next_links(url, resp):
-    # Implementation required.
-    webResponse = BeautifulSoup(resp.raw_response.content, 'html.parser')
-    tags = webResponse.find_all('a')
+    # Implementation requred.
+    webResponse=BeautifulSoup(resp.raw_response.content,'html.parser')
+    tags=webResponse.find_all('a')
+    urlList=[]
     for tag in tags:
-        print(tag.get('href'))
-
-    return tag
-
-
+        urlList.append(tag.get('href'))
+    return urlList
 def tokenize(TextFilePath):
-    # Tokenizes a text file looking for an sequence of 2+ alphanumerics while ignoring stop words
-    regularPattern = '[A-Za-z0-9]{2,}'
-    stopWords = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't",
-                 "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by",
-                 "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't",
-                 "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have",
-                 "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him",
-                 "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't",
-                 "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor",
-                 "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out",
-                 "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so",
-                 "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then",
-                 "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those",
-                 "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll",
-                 "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which",
-                 "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd",
-                 "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"}
+
+    regularPattern= '[A-Za-z0-9]{2,}'
+
     try:
-        file = open(TextFilePath, 'r', encoding='utf-8')
-        tokenList = []
-        lines = file.readlines()
-        # for each line in the file, we find the appropriate regex
+        file = open(TextFilePath, 'r',encoding='utf-8')
+        tokenList=[]
+        lines=file.readlines()
+        #for each line in the file, we find the appropriate regex
         for line in lines:
-            line = line.lower()
-            wordlist = re.findall(regularPattern, line)
-            templist = wordlist
-            for word in templist:
-                if word in stopWords:
-                    wordlist.remove(word)
+            line=line.lower()
+            wordlist=re.findall(regularPattern,line)
             tokenList.extend(wordlist)
         return tokenList
         file.close()
@@ -60,30 +38,9 @@ def tokenize(TextFilePath):
         file.close()
         sys.exit()
 
-
-def computeWordFrequencies(Tokens):
-    # Take list of tokens and return a dictionary with tokens and the number of occurrences of each token
-    # also updates the DBDictionary with the tokens found on this page
-    output = dict()
-    for word in Tokens:
-        output[word] = output.get(word, 0) + 1
-        DBDictionary[word] = DBDictionary.get(word, 0) + 1
-    return output
-
-
-def Top50(wordDict):
-    # takes the DBDictionary and prints the 50 most common words in dictionary
-    sortedWords = sorted(wordDict.items(), key=lambda x: 0 - x[1])
-    n = 50;
-    for word, val in sortedWords:
-        if n > 0:
-            print("<" + word + "> <" + str(wordDict[word]) + ">")
-            n -= 1
-        else:
-            break
-
 def is_valid(url):
     try:
+        print(url)
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
@@ -98,5 +55,5 @@ def is_valid(url):
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
 
     except TypeError:
-        print("TypeError for ", parsed)
+        print ("TypeError for ", parsed)
         raise
