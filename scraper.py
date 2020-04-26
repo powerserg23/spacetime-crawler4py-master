47486e20007b4460a5bc4a2a5558dfa595dc97b2
bqdang1 <dang.q.brian@gmail.com>
2020-04-25 20:24:29 -0700

    minor changes to the format of output file

--- scraper.py
+++ scraper.py
@@ -168,7 +168,7 @@ def is_valid(url):
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
+            + r"|epub|dll|cnf|tgz|sha1|php|z"
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz|ppt|pptx|ppsx"
             + r"|january|february|march|april|may|june|july"
